c5ce460269a49768c1eae33b0371133bd00698ab
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2021-06-04 18:34:54 +0300

    Marker placement on map backend

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ window.onload = function () {
     span = document.getElementsByClassName("modal-close")[0];
 
     map_init();
+    getData();
 
     span.onclick = function () {
         modal.style.display = "none";
@@ -30,7 +31,7 @@ function modalBody(body) {
 }
 
 function map_init() {
-    mymap = L.map("mapid").setView([35.25, 24.93], 13);
+    mymap = L.map("mapid").setView([35.25, 24.93], 9);
 
     L.tileLayer(
         "https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.{ext}",
@@ -39,7 +40,7 @@ function map_init() {
                 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
             subdomains: "abcd",
             minZoom: 0,
-            maxZoom: 9,
+            maxZoom: 11,
             ext: "png",
         }
     ).addTo(mymap);
@@ -51,9 +52,22 @@ function getData() {
     xhrObj.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             dataJSON = JSON.parse(this.responseText);
+
+            place_markers(mymap, dataJSON);
         }
     };
 
     xhrObj.open("GET", "data.json", false);
     xhrObj.send();
+}
+
+function place_markers(map, data) {
+    data.forEach((element) => {
+        let marker = L.marker(element["location"]).addTo(map);
+        marker.on("click", () => {
+            modalHead(element["title"]);
+            modalBody(element["body"]);
+            modal.style.display = "block";
+        });
+    });
 }
